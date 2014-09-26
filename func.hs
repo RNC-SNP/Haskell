06408cabce574f6391dfd382eb2814a11af98823
Rinc <i@RincLiu.com>
2014-09-25 22:20:37 -0700

    Added comments.

--- func.hs
+++ func.hs
@@ -1,7 +1,7 @@
-funcSquare :: Double -> Double
-funcSquare x = x*x
+funcSquare :: Double -> Double --Declare function's type 
+funcSquare x = x*x --Declare function
 funcSum :: Num a => a -> a -> a
 funcSum x y = x+y
 main = do
-	print (funcSquare 0.123456789)
+	print (funcSquare 0.123456789) --Call function
 	print (funcSum 8 9)
