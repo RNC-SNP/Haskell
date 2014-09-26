7c3417335dcaf6ce9771f01328d638c47b8fbbed
Rinc <i@RincLiu.com>
2014-09-25 22:51:23 -0700

    Pattern-Match and Recursion.

--- func.hs
+++ func.hs
@@ -1,7 +1,14 @@
 funcSquare :: Double -> Double --Declare function's type 
 funcSquare x = x*x --Declare function
-funcSum :: Num a => a -> a -> a
+
+funcSum :: (Num a) => a -> a -> a --Any type
 funcSum x y = x+y
+
+funcFactorial :: (Integral a) => a -> a
+funcFactorial 0 = 1 --Pattern Match
+funcFactorial n = n*funcFactorial(n-1) --Recursion
+
 main = do
-	print (funcSquare 0.123456789) --Call function
-	print (funcSum 8 9)
+	print(funcSquare 0.123456789) --Call function
+	print(funcSum 8 9)
+	print(funcFactorial 321)
