4338272bbd195a1c8dc191a5541833ec603bce2c
Rinc <i@RincLiu.com>
2014-09-25 23:37:06 -0700

    Use 'error' & 'x'.

--- func.hs
+++ func.hs
@@ -12,8 +12,13 @@ funcFabonacci n = funcFabonacci(n-1)+funcFabonacci(n-2) --Recursion
 funcAddTuple :: (Num a) => (a,a) -> (a,a) -> (a,a) --Use tuple in function
 funcAddTuple a b = (fst a + fst b, snd a + snd b) --'fst' & 'snd' has higher priority than '+'
 
+funcHead :: [a] -> a
+funcHead [] = error "Cannot call this function on empty list!" --Use error
+funcHead (x:_) = x --'x' is the 1st element
+
 main = do
 	print(funcSquare 0.123456789) --Call function
 	print(funcSum 8 9)
 	print(funcFabonacci 10)
 	print(funcAddTuple (1,2) (0.1,0.2))
+	print(funcHead [1,2,3,4,5])
