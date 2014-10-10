9e831c89d97fd949965dc2d3753e61aa0857f756
RincLiu <i@RincLiu.com>
2014-10-09 20:25:29 -0700

    Define data type.

--- data-type.hs
+++ data-type.hs
@@ -1,11 +1,12 @@
 -- Define data type
-data Shape = Circle Float Float Float | Retangle Float Float Float Float
+data Point = Point Float Float deriving (Show)
+data Shape = Circle Point Float | Retangle Point Point deriving (Show)
 
 -- Use custom data type in function
 surface :: Shape -> Float
-surface (Circle _ _ r) = pi * r ^ 2
-surface (Retangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
+surface (Circle _ r) = pi * r ^ 2
+surface (Retangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
 
 main = do
-	print(surface (Circle 0 0 1.23456789))
-	print(surface (Retangle (-1.0) (-2.0) 1.0 2.0))
+	print(surface (Circle (Point 0 0) 1.23456789))
+	print(surface (Retangle (Point (-1.0) (-2.0)) (Point 1.0 2.0)))
