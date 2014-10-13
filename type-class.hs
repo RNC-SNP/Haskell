54ed1e3a29e00661ec4251023f0c58bad1123421
RincLiu <i@RincLiu.com>
2014-10-12 22:47:13 -0700

    Custom TypeClass.

--- type-class.hs
+++ type-class.hs
@@ -13,6 +13,31 @@ instance Show Gender where
 	show Female = "Female"
 	show Other = "Other"
 
+
+
+-- Define a TypeClass
+class YesNo a where
+	yesno :: a -> Bool -- Define functions in this TypeClass
+
+-- Implements YesNo TypeClass
+instance YesNo Int where
+	yesno 0 = False
+	yesno _ = True
+
+instance YesNo Bool where
+	yesno = id -- A standard lib-func which takes a param and returns the same thing.
+
+instance YesNo (Maybe a) where
+	yesno (Just _) = True
+	yesno Nothing = False
+
+
+
 main = do
 	print(Male == Female)
+	print(Male /= Female)
 	print(show Male)
+
+	--print(yesno 1)
+	print(yesno False)
+	print(yesno $ Just "Rinc")
