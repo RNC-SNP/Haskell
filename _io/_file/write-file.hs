21e18d727c616fdd8a33089e3df59067fdcccdf1
RincLiu <i@RincLiu.com>
2014-10-13 03:19:44 -0700

    Rename & remove file.

--- _io/_file/write-file.hs
+++ _io/_file/write-file.hs
@@ -1,6 +1,9 @@
 import System.IO
+import System.Directory
 import Data.Char
 
 main = do
 	contents <- readFile "write-file.hs"
 	writeFile "new-file.txt" (map toUpper contents)
+	renameFile "new-file.txt" "new-file.txt.txt"
+	removeFile "new-file.txt.txt"
