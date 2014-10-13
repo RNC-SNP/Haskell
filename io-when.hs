9b9ced1000fe501586433f417da97bbed0a1007e
RincLiu <i@RincLiu.com>
2014-10-13 01:29:44 -0700

    when

--- io-when.hs
+++ io-when.hs
@@ -2,6 +2,6 @@ import Control.Monad
 
 main = do
 	c <- getChar
-	when (c /= ' ') $ do -- return () where the expression is False
+	when (c /= ' ') $ do -- return () where the bool-expression's value is False
 		putChar c
 		main
