0a9ac2ec970e4d3115526c6e86e9544a1a75081c
RincLiu <i@RincLiu.com>
2014-10-13 02:25:58 -0700

    interact

---
+++ _io/io-interact.hs
@@ -0,0 +1,12 @@
+shortLinesOnly :: String -> String
+shortLinesOnly input =
+	let allLines = lines input
+	    shortLines = filter (\line -> length line < 10) allLines
+	    result = unlines shortLines
+	in result
+
+--main = do
+--	contents <- getContents
+--	putStr(shortLinesOnly contents)
+
+main = interact shortLinesOnly
